871758a951472293e9bfe1c1f2c90fce8b6fffbd
phil.zhang <phil.zhang@chinascopefinancial.com>
2016-08-17 14:00:54 +0800

    add multiple factors analysis

--- factors/multiple_factor_analysis.py
+++ factors/multiple_factor_analysis.py
@@ -1,6 +1,7 @@
 # coding: utf8
 import pandas as pd
 
+from factors.data_type import FactorData
 from factors.metrics import information_coefficient
 
 
@@ -121,3 +122,36 @@ def score(fac_ret_data, method='equal_weighted', asending=False, rank_method='fi
     except KeyError:
         print('{} is not a valid method. valid methods are: {}'.format(method, valid_method.keys()))
         raise
+
+
+def multiple_factors_analysis(data, pipeline):
+    """
+    多因子分析
+    Args:
+        data (DataFrame): 一个multi_index 数据框, 有因子, 对应下期收益率, 市值列. multi_index-->level0=dt, level1=code
+        pipeline(List): 前面N-1个为对数据进行处理, 最后一个元素为一个元组,元组的元素为xxx_analysis
+
+    Returns:
+        多因子分析结果
+    Examples:
+        from analysis import filter_out_st, filter_out_suspend, filter_out_recently_ipo
+        from analysis import prepare_data, add_group, de_extreme
+        from analysis import information_coefficient_analysis, return_analysis, code_analysis, turnover_analysis
+
+        data = prepare_data(factor_name=["M004009", "M004023"], index_code='000300', benchmark_code='000300',
+        start_date='2015-01-01', end_date='2016-01-01, freq='M')
+        pipeline = [filter_out_st, filter_out_suspend, filter_out_recently_ipo,de_extreme, standardize, score, add_group,
+        (information_coefficient_analysis, return_analysis, code_analysis, turnover_analysis)]
+        result = single_factor_analysis(pipeline)
+    """
+    X = data.copy()
+    for func in pipeline[:-1]:
+        X = func(X)
+
+    result_dict = {}
+    for func in pipeline[-1]:
+        result_dict[func.__name__] = func(X)
+
+    # factor_name = get_factor_name(data)
+    factor_result = FactorData(name='multiple', **result_dict)
+    return factor_result
